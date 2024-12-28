6563aefb4551af683de24e16eb89392db392285c
vijayvk10 <vijayakrishnacvk10@gmail.com>
2024-12-28 11:46:11 +0530

    'ver2'

---
+++ fs.js
@@ -0,0 +1,43 @@
+var a = 10
+var b = 10
+
+if(a==b)
+{
+    console.log("both values are same")
+}
+else{
+    console.log("both values are not same")
+}
+
+//arithmetic operators
+add=a+b
+console.log(add)
+sub=a-b
+console.log(sub)
+mul=a*b
+console.log(mul)
+div=a/b
+console.log(div)
+mod=a%b
+console.log(mod)
+
+//assignment operators
+b += 10; 
+console.log("After += Operator:", b); 
+
+//comparison operators
+console.log("a > b:", a > b); 
+console.log("a == b:", a == b); 
+console.log("a === b:", a === b); 
+
+//logical operators
+let x = true, y = false;
+console.log("x && y:", x && y); 
+console.log("x || y:", x || y); 
+console.log("!x:", !x);  
+
+//unary operators
+console.log("Increment:", ++b); 
+console.log("Decrement:", --b); 
+
+
